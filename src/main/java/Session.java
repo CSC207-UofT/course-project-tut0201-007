594b8f5a634667d22fd979f5f8510a8e179ed921
anton <asugolov@gmail.com>
2021-10-11 22:07:09 -0400

    Added Schedule class, added comments in Session class.

--- src/main/java/Session.java
+++ src/main/java/Session.java
@@ -2,6 +2,8 @@
  * This class represents a session, which occupies a timeslot on a schedule
  */
 public class Session {
+
+    //TODO: Add an instance variable representing the time of the Session.
     private final String sessionId;
     private final String assignedRoom;
 
@@ -14,4 +16,21 @@ public class Session {
         this.sessionId = sessionId;
         this.assignedRoom = assignedRoom;
     }
+
+    /**
+     *
+     */
+    @Override
+    public String toString() {
+        return this.sessionId + " in " + this.assignedRoom;
+        //TODO: Add the time to the representation
+    }
+
+    //TODO: Make these comparable with respect to time.
+    /**
+     * If s1, s2 are sessions, then
+     * s1 < s2 iff s1 is before s2, and they do not conflict
+     * s1 == s2 iff s1 and s2 conflict
+     * s1 > s2 iff s1 is after s2 and they do not conflict.
+     */
 }
