ddfc96534104408ea396c6c9295c6968d51531ed
anton <asugolov@gmail.com>
2021-10-13 00:18:56 -0400

    Added new initializer and method to Scheduler.

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 
 public class Scheduler {
+    private final ArrayList courses;
     private final ArrayList schedules;
 
     public Scheduler() {
+        this.courses = new ArrayList<>();
         this.schedules = new ArrayList<>();
     }
+
+    public Scheduler(ArrayList<Course> courses, ArrayList<Schedule> schedules) {
+        this.courses = courses;
+        this.schedules = schedules;
+    }
+
+    public Schedule generateSchedule() {
+        return new Schedule();
+    }
+
+
 }
