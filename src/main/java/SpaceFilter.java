9ef4a1fb93b1dc8e53cbfc86f8f12bf21b56c932
EclipseIsDead <sidmcheath@gmail.com>
2021-11-11 19:18:38 -0500

    Updated documentation

--- src/main/java/SpaceFilter.java
+++ src/main/java/SpaceFilter.java
@@ -5,15 +5,22 @@ import java.util.ArrayList;
  * It is made "stackable" by returning the schedule if it works, and then that can be used for further calls.
  */
 public class SpaceFilter implements Filter {
-    // This is the interval between classes that you want, in hours
     private final int interval;
 
+    /**
+     * Constructor that assigns int for hours as the interval
+     *
+     * @param hours is the hour(s) of "buffer" you would like between classes
+     */
     public SpaceFilter(int hours) {
         this.interval = hours;
     }
-
-    // Essentially, we are returning nothing if the schedule does not pass the filter,
-    // otherwise return the schedule so we can layer
+    /**
+     * Essentially, we are returning nothing if the schedule does not pass the filter,
+     * otherwise return the schedule, so we can layer
+     *
+     * @param s is a schedule we want to check follows the "interval" rule
+     */
     @Override
     public Schedule checkSchedule(Schedule s) {
         // quick null type check
