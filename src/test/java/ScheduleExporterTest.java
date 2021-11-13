08cf2868139d2f77f635a4be6ed81b2a5789e082
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-11-12 20:50:26 -0500

    reverted but printing

--- src/test/java/ScheduleExporterTest.java
+++ src/test/java/ScheduleExporterTest.java
@@ -32,7 +32,31 @@ public class ScheduleExporterTest {
         String writtenICS = writer.toString();
         System.out.println("bruh");
 
-        Schedule output = ScheduleImporter.importSchedule(new StringReader(writtenICS));
+        String dummyICS =
+                "BEGIN:VCALENDAR\n"
+                        + "PRODID:-//CSC207 Team 007//iCal4j 1.0//EN\n"
+                        + "VERSION:2.0\n"
+                        + "CALSCALE:GREGORIAN\n"
+                        + "BEGIN:VEVENT\n"
+                        + "DTSTAMP:20211112T222803Z\n"
+                        + "DTSTART:20210910T180000\n"
+                        + "DTEND:20210910T210000\n"
+                        + "SUMMARY:TST101 TUT-0101 F\n"
+                        + "RRULE:FREQ=WEEKLY;UNTIL=20211210\n"
+                        + "UID:ed9cb5e6-c588-41e6-810e-e4d98b072b53\n"
+                        + "LOCATION:ROOM 05\n"
+                        + "END:VEVENT\n"
+                        + "BEGIN:VEVENT\n"
+                        + "DTSTAMP:20211112T222803Z\n"
+                        + "DTSTART:20210913T120000\n"
+                        + "DTEND:20210913T130000\n"
+                        + "SUMMARY:TST101 LEC-0101 F\n"
+                        + "RRULE:FREQ=WEEKLY;UNTIL=20211210\n"
+                        + "UID:4fc6f442-67ba-48dc-8c34-2c30709a1d57\n"
+                        + "LOCATION:ROOM 07\n"
+                        + "END:VEVENT\n"
+                        + "END:VCALENDAR";
+        Schedule output = ScheduleImporter.importSchedule(new StringReader(dummyICS));
         assert (output.equals(coursesSchedule));
     }
 }
