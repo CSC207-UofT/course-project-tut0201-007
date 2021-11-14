071d22ceb9a79a69b3a76fa4fb7d9703f2ca5f28
Baker Jackson <bakerwjackson@gmail.com>
2021-11-14 18:18:08 -0500

    Add helpers for prompting user input

---
+++ src/main/java/util/PromptHelpers.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class PromptHelpers {
+    public static ArrayList<String> promptUserStrings(String question, int num) {
+        ArrayList<String> ret = new ArrayList<>();
+        for (int a = 0; a < num; a++) {
+            ret.add(promptUserString(question));
+        } 
+        return ret;
+    }
+
+    public static int promptUserInt(String question) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean input = false;
+        int numCourses = 0;
+        while (!input) {
+            System.out.println("How many courses would you like information for?");
+            try {
+                numCourses = Integer.parseInt(scanner.nextLine());
+                input = true;
+
+            } catch (Exception NumberFormatException) {
+                System.out.println("Please give an integer input");
+            }
+        }
+        scanner.close();
+        return numCourses;
+    }
+
+    public static String promptUserString(String question) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+}
