65fb152069b5bf85611f829a782c944fbf04930c
Baker Jackson <bakerwjackson@gmail.com>
2021-11-14 19:13:33 -0500

    Add helpers for prompting user input (#51)
    * Add helpers for prompting user input
    
    * apply spotless
    
    * Add method for list of integers

---
+++ src/main/java/util/PromptHelpers.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+class PromptHelpers {
+    public static List<String> promptUserStrings(String question, int num) {
+        ArrayList<String> ret = new ArrayList<>();
+        for (int a = 0; a < num; a++) {
+            ret.add(promptUserString(question));
+        }
+        return ret;
+    }
+
+    public static int promptUserInt(String question) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean input = false;
+        int numCourses = 0;
+        while (!input) {
+            System.out.println("How many courses would you like information for?");
+            try {
+                numCourses = Integer.parseInt(scanner.nextLine());
+                input = true;
+
+            } catch (Exception NumberFormatException) {
+                System.out.println("Please give an integer input");
+            }
+        }
+        scanner.close();
+        return numCourses;
+    }
+
+    public static List<Integer> promptUserInts(String question, int num) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        for (int a = 0; a < num; a++) {
+            ret.add(promptUserInt(question));
+        }
+        return ret;
+    }
+
+    public static String promptUserString(String question) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+}
