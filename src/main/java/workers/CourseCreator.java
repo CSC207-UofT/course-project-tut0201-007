3ee60c90bfc444697ebbff05c2d2b40ce25493b4
Kenneth Miura <kenneth.t.miura@gmail.com>
2021-11-14 20:29:23 -0800

    handling case of winter only course (#52)

--- src/main/java/workers/CourseCreator.java
+++ src/main/java/workers/CourseCreator.java
@@ -41,7 +41,7 @@ public class CourseCreator {
     public static Course generateCourse(String courseId, char session) throws IOException {
         APIWorker apiWorker = new APIWorker(courseId);
         int w = 0;
-        if (session == 'S') {
+        if (session == 'S' && apiWorker.semester.size() == 2) {
             w = 1;
         }
         JsonObject meetings =
