31041e90fb462b5b05628fca3d835cb3273b8fff
Rory Hofland <roryhofland@gmail.com>
2021-11-23 18:14:42 -0500

    added comments

--- src/main/java/workers/Importer.java
+++ src/main/java/workers/Importer.java
@@ -7,6 +7,12 @@ import java.io.Reader;
 /** An abstract class that provides a template for importing schedules with different file types (i.e. ICS, CSV, etc.) */
 public abstract class Importer {
 
+    /**
+     * An abstract method requiring that any import class has a method to import a Schedule object from a file
+     *
+     * @param reader The Reader object that parses from the file
+     * @return A Schedule object that contains all the Sections from the file
+     */
     public abstract Schedule importSchedule(Reader reader);
 
 }
