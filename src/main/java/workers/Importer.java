cc5f342dc6431e336edae3c93129c3951290a962
Rory Hofland <roryhofland@gmail.com>
2021-11-25 21:01:57 -0500

    made Importer an interface

--- src/main/java/workers/Importer.java
+++ src/main/java/workers/Importer.java
@@ -5,14 +5,14 @@ import entities.Schedule;
 import java.io.Reader;
 
 /** An abstract class that provides a template for importing schedules with different file types (i.e. ICS, CSV, etc.) */
-public abstract class Importer {
+public interface Importer {
 
     /**
-     * An abstract method requiring that any import class has a method to import a Schedule object from a file
+     * A method requiring that any import class has a method to import a Schedule object from a file
      *
      * @param reader The Reader object that parses from the file
      * @return A Schedule object that contains all the Sections from the file
      */
-    public abstract Schedule importSchedule(Reader reader);
+    Schedule importSchedule(Reader reader);
 
 }
