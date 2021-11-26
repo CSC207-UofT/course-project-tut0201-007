a9e919e2fafc4a7de42edfc2b1d5ad73801c7c8b
Rory Hofland <roryhofland@gmail.com>
2021-11-26 14:22:12 -0500

    added csv functionality

--- src/main/java/workers/CSVExporter.java
+++ src/main/java/workers/CSVExporter.java
@@ -3,24 +3,14 @@ package workers;
 import entities.Schedule;
 import entities.Section;
 import entities.Timeslot;
-import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Date;
-import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.Recur;
-import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.property.*;
-import net.fortuna.ical4j.util.RandomUidGenerator;
-import net.fortuna.ical4j.util.UidGenerator;
-import util.InvalidSessionException;
-
 import java.io.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
+import util.InvalidSessionException;
 
-public class CSVExporter extends Exporter{
+public class CSVExporter extends Exporter {
 
-    private int numFiles = 0;
+    private int num = 0;
     private final ZoneId zoneId = ZoneId.of("-4");
     private final LocalDate now = LocalDate.now(zoneId);
     private final int startYear = now.getMonthValue() < 9 ? now.getYear() - 1 : now.getYear();
@@ -31,30 +21,29 @@ public class CSVExporter extends Exporter{
 
     @Override
     public void outputSchedule(Schedule schedule) {
-        String baseDir = new File("").getAbsolutePath();
-        File outputDir = new File(baseDir.concat("/output"));
+        File outputDir = new File(new File("").getAbsolutePath().concat("/output"));
         if (!outputDir.exists()) {
             outputDir.mkdir();
         }
         try {
             Writer writer =
                     new FileWriter(
-                            outputDir
-                                    .getAbsolutePath()
-                                    .concat("/CSVSchedule_" + numFiles + ".csv"));
+                            outputDir.getAbsolutePath().concat("/CSVSchedule_" + num + ".csv"));
             generateCSVSchedule(schedule, writer);
-            numFiles += 1;
+            num += 1;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private void generateCSVSchedule(Schedule schedule, Writer writer) {
+        StringBuilder rawSchedule = new StringBuilder();
+        rawSchedule.append("Subject,Start Date,Start Time,End Date,End Time,Location\n");
         for (Section tutorial : schedule.getTutorials()) {
             for (Timeslot timeslot : tutorial.getTimes()) {
                 try {
-                    scheduleTimeslot(
-                            tutorial.getName(), tutorial.getSession(), timeslot);
+                    rawSchedule.append(
+                            scheduleTimeslot(tutorial.getName(), tutorial.getSession(), timeslot));
                 } catch (InvalidSessionException e) {
                     e.printStackTrace();
                 }
@@ -63,8 +52,8 @@ public class CSVExporter extends Exporter{
         for (Section lecture : schedule.getLectures()) {
             for (Timeslot timeslot : lecture.getTimes()) {
                 try {
-                    scheduleTimeslot(
-                            lecture.getName(), lecture.getSession(), timeslot);
+                    rawSchedule.append(
+                            scheduleTimeslot(lecture.getName(), lecture.getSession(), timeslot));
                 } catch (InvalidSessionException e) {
                     e.printStackTrace();
                 }
@@ -72,50 +61,47 @@ public class CSVExporter extends Exporter{
         }
     }
 
-    private void scheduleTimeslot(String name, String session, Timeslot timeslot) throws InvalidSessionException {
-            LocalDate termStartDate, termEndDate;
-            switch (session) {
-                case "F":
-                    termStartDate = FALL_SEMESTER_START_DATE;
-                    termEndDate = FALL_SEMESTER_END_DATE;
-                    break;
-                case "S":
-                    termStartDate = WINTER_SEMESTER_START_DATE;
-                    termEndDate = WINTER_SEMESTER_END_DATE;
-                    break;
-                case "Y":
-                    termStartDate = FALL_SEMESTER_START_DATE;
-                    termEndDate = WINTER_SEMESTER_END_DATE;
-                    break;
-                default:
-                    throw new InvalidSessionException(
-                            String.format(
-                                    "%s has invalid session %s. It should be either F, Y, or S",
-                                    name, session));
-            }
-            Date finalDay = new Date(java.util.Date.from(termEndDate.atStartOfDay(zoneId).toInstant()));
-
-            UidGenerator ug = new RandomUidGenerator();
-            Uid uid = ug.generateUid();
-            Recur recur = new Recur(Recur.Frequency.WEEKLY, finalDay);
-            RRule recurrenceRule = new RRule(recur);
-
-            LocalDate startDay = getStartingWeekDate(termStartDate, timeslot.getDay());
-            LocalDateTime start = startDay.atTime(timeslot.getStart());
-            Date timeslotStart = new DateTime(java.util.Date.from(start.atZone(zoneId).toInstant()));
-
-            LocalDateTime end = startDay.atTime(timeslot.getEnd());
-            Date timeslotEnd = new DateTime(java.util.Date.from(end.atZone(zoneId).toInstant()));
-
-            Location location = new Location(timeslot.getRoom());
-
-            VEvent event = new VEvent(timeslotStart, timeslotEnd, name);
-            event.getProperties().add(recurrenceRule);
-            event.getProperties().add(uid);
-            event.getProperties().add(location);
-
-            calendar.getComponents().add(event);
+    private String scheduleTimeslot(String name, String session, Timeslot timeslot)
+            throws InvalidSessionException {
+        LocalDate termStartDate, termEndDate;
+        switch (session) {
+            case "F":
+                termStartDate = FALL_SEMESTER_START_DATE;
+                termEndDate = FALL_SEMESTER_END_DATE;
+                break;
+            case "S":
+                termStartDate = WINTER_SEMESTER_START_DATE;
+                termEndDate = WINTER_SEMESTER_END_DATE;
+                break;
+            case "Y":
+                termStartDate = FALL_SEMESTER_START_DATE;
+                termEndDate = WINTER_SEMESTER_END_DATE;
+                break;
+            default:
+                throw new InvalidSessionException(
+                        String.format(
+                                "%s has invalid session %s. It should be either F, Y, or S",
+                                name, session));
         }
-    }
 
+        StringBuilder rawTimeslot = new StringBuilder();
+        LocalDate currDay = getStartingWeekDate(termStartDate, timeslot.getDay());
+        while (currDay.isBefore(termEndDate)) {
+            rawTimeslot
+                    .append(name)
+                    .append(",")
+                    .append(currDay)
+                    .append(",")
+                    .append(timeslot.getStart())
+                    .append(",")
+                    .append(currDay)
+                    .append(",")
+                    .append(timeslot.getEnd())
+                    .append(",")
+                    .append(timeslot.getRoom())
+                    .append("\n");
+            currDay = currDay.plusDays(7);
+        }
+        return rawTimeslot.toString();
+    }
 }
