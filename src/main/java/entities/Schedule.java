4ea7900a6c527d8b10814bf449ea6383bb2aac5d
EclipseIsDead <sidmcheath@gmail.com>
2021-11-25 20:45:43 -0500

    spotless

--- src/main/java/entities/Schedule.java
+++ src/main/java/entities/Schedule.java
@@ -31,7 +31,7 @@ public class Schedule implements Cloneable {
         this.courses = new ArrayList<>();
 
         for (Section sec : this.lectures) {
-            String courseCode = sec.getName().substring(0,6);
+            String courseCode = sec.getName().substring(0, 6);
             if (!this.courses.contains(courseCode)) {
                 courses.add(courseCode);
             }
@@ -58,7 +58,7 @@ public class Schedule implements Cloneable {
     public void addLecture(Section lecture) {
         lectures.add(lecture);
 
-        String courseCode = lecture.getName().substring(0,6);
+        String courseCode = lecture.getName().substring(0, 6);
         if (!this.courses.contains(courseCode)) {
             this.courses.add(courseCode);
         }
@@ -101,7 +101,6 @@ public class Schedule implements Cloneable {
         Schedule clone = (Schedule) super.clone();
         return new Schedule(
                 (ArrayList<Section>) this.lectures.clone(),
-                (ArrayList<Section>) this.tutorials.clone()
-        );
+                (ArrayList<Section>) this.tutorials.clone());
     }
 }
