3b5a324abd422923c18806ee8cea2ef63a7bf283
Rory Hofland <roryhofland@gmail.com>
2021-12-02 14:19:32 -0500

    First commit

---
+++ src/main/java/workers/CSVImporter.java
@@ -0,0 +1,44 @@
+package workers;
+
+import entities.Schedule;
+import entities.Section;
+import entities.Timeslot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+
+public class CSVImporter implements Importer{
+
+    @Override
+    public Schedule importSchedule(Reader reader) {
+        BufferedReader br = new BufferedReader(reader);
+        Schedule schedule = new Schedule();
+        Map<String, Section> sectionsByName = new HashMap<>();
+
+        try {
+            String line;
+            while((line=br.readLine())!=null)
+            {
+                List<String> contents = Arrays.asList(line.split(","));
+                if (!contents.get(0).equals("Subject") && !sectionsByName.containsKey(contents.get(0))) {
+                    Timeslot timeslot =
+                            new Timeslot(
+                                    LocalTime.parse(contents.get(2)),
+                                    LocalTime.parse(contents.get(4)),
+                                    LocalDateTime.parse(contents.get(1)).getDayOfWeek(),
+                                    contents.get(5),
+                                    contents.get(5).charAt(-1));
+                    sectionsByName.put(contents.get(0), timeslot);
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
