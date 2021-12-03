e8f3127b7853f41c1e0c2d6d0db2384465425b16
Baker Jackson <bakerwjackson@gmail.com>
2021-12-03 12:17:49 -0500

    improve rate my prof runtime with hashmap

--- src/main/java/workers/ProfessorRatingScraper.java
+++ src/main/java/workers/ProfessorRatingScraper.java
@@ -3,29 +3,35 @@ package workers;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class ProfessorRatingScraper {
+    private static Map<String, Double> previousScores = new HashMap<String, Double>();
+
     public static double getRating(String firstName, String lastName) {
+        if (previousScores.containsKey(firstName + lastName)) {
+            return previousScores.get(firstName + lastName);
+        }
         String html;
         try {
-            html =
-                    Jsoup.connect(
-                                    "https://www.ratemyprofessors.com/search/teachers?query="
-                                            + firstName
-                                            + " "
-                                            + lastName
-                                            + "&sid=U2Nob29sLTE0ODQ=")
-                            .get()
-                            .html();
+            html = Jsoup.connect(
+                    "https://www.ratemyprofessors.com/search/teachers?query="
+                            + firstName
+                            + " "
+                            + lastName
+                            + "&sid=U2Nob29sLTE0ODQ=")
+                    .get()
+                    .html();
         } catch (IOException e) {
             return 2.5;
         }
         Document doc = Jsoup.parse(html);
-        Element teacher =
-                doc.getElementsByClass("TeacherCard__StyledTeacherCard-syjs0d-0 dLJIlx").first();
+        Element teacher = doc.getElementsByClass("TeacherCard__StyledTeacherCard-syjs0d-0 dLJIlx").first();
         Double ret = -1.0;
         Boolean correctSchool = false;
         if (teacher == null) {
+            previousScores.put(firstName + lastName, 2.5);
             return 2.5;
         }
         for (Element e : teacher.getElementsByTag("div")) {
@@ -38,9 +44,11 @@ class ProfessorRatingScraper {
                 }
             }
             if (correctSchool && ret != -1.0) {
+                previousScores.put(firstName + lastName, ret);
                 return ret;
             }
         }
+        previousScores.put(firstName + lastName, 2.5);
         return 2.5;
     }
 }
