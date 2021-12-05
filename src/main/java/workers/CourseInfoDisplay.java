93245e76ca564b080270c57c3751800981ef857d
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-12-04 19:38:43 -0500

    Create CourseInfoDisplay.java

---
+++ src/main/java/workers/CourseInfoDisplay.java
@@ -0,0 +1,15 @@
+package workers;
+
+import entities.Course;
+
+/**
+ * A class that allows course information to be displayed to the user
+ */
+public class CourseInfoDisplay {
+
+    public CourseInfoDisplay(Course course) {
+
+    }
+
+
+}
